be05a36b9afa44c5c22b8670a06dcfa15abfdbec
viktech <vikrez2021@gmail.com>
2025-09-26 02:17:12 +0100

    logout ui/ux updated

--- src/components/Dashboard/HerderDashboard.js
+++ src/components/Dashboard/HerderDashboard.js
@@ -329,12 +329,12 @@ const HerderDashboard = ({ userId }) => {
 
         {/* Footer */}
         {/* //logout button here */}
-          <a href="/login"> <button
-            className={`${sidebarOpen ? "w-full" : "w-fit"} bg-red-100 m-3 flex items-center gap-3 px-3 py-2 rounded-lg text-red-600 hover:bg-red-50 transition-colors`}
-          >
-            <X size={20} />
-            <span className={`${sidebarOpen ? "block" : "hidden"} lg:block font-medium`}>Logout</span>
-          </button></a>
+        <a href="/login"> <button
+          className={"sidebarOpen w-60 bg-red-100 flex m-3 items-center gap-3 px-3 py-2 rounded-lg text-red-600 hover:bg-red-50 transition-colors"}
+        >
+          <X size={20} />
+          <span className={`${sidebarOpen ? "block" : "hidden"} lg:block font-medium`}>Logout</span>
+        </button></a>
         <div className="p-4 border-t border-gray-200">
           <div className={`${sidebarOpen ? "block" : "hidden"} lg:block`}>
             <div className="text-center">
