8530a1da65216520f6beb35d232db4c6f3ce2556
viktech <vikrez2021@gmail.com>
2025-11-20 19:19:23 +0100

    Registered the AIRoute in server.js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import userRoutes from './routes/user.js'
+import aiRoutes from './routes/ai.js'
 import 'dotenv/config'
 
 
@@ -13,6 +14,7 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/api/users', userRoutes)
+app.use('/api/ai', aiRoutes);
 
 
 app.get('/health', (req, res) => {
