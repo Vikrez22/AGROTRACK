0bd6275debaaa054ec43a330299dcdca7e7a198a
Munachi <munaonye4@gmail.com>
2025-11-26 19:05:58 +0100

    adding the requests page

---
+++ frontend/src/components/Dashboard/FarmerDashboard/Requests.jsx
@@ -0,0 +1,10 @@
+const Requests = () => {
+  return (
+    <div className="text-5xl">
+      <div>
+        <div className="bg-white border w-65 h-55"></div>
+      </div>
+    </div>
+  );
+};
+export default Requests;
